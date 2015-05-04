afba3b2f8e73c72ab342887829089b61a7569455
Chad Heuschober <chad.heuschober@cuny.edu>
2015-05-03 20:36:45 -0400

    Fixes docstring for pet.

--- pet.py
+++ pet.py
@@ -9,8 +9,23 @@ class Pet(object):
     """A class that represents a generic Pet."""
 
     def __init__(self, birthyear, weight, length, color, owner=None):
-        """Constructor for the Pet class."""
+        """Constructor for the Pet class.
+        
+        Args:
+            birthyear (int): Year the pet was born.
+            weight (int): Weight of the pet in kg.
+            length (int): Length of the pet in cm.
+            color (str): Color of the pet.
+            owner (str, optional): Name of the pet's owner, defaults to None.
 
+        Attributes:
+            age: (int): Age of the pet in years.
+            birthyear (int): Year the pet was born.
+            weight (int): Weight of the pet in kg.
+            length (int): Length of the pet in cm.
+            color (str): Color of the pet.
+            owner (str, optional): Name of the pet's owner, defaults to None.    
+        """
         self.age = datetime.datetime.now().year - birthyear
         self.birthyear = birthyear
         self.weight = weight
